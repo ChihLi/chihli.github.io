f2623a611d02c3defcbdf968ead9fcb64b77fa75
Chih-Li Sung <iamdfchile@gmail.com>
2022-10-12 00:05:13 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,22 @@
 var addressPoints = [
   [
+    "Washington DC",
+    38.8950368,
+    -77.0365427
+  ],
+  [
     "Greensboro, North Carolina",
     36.0726355,
     -79.7919754
+  ],
+  [
+    "Hsinchu, Taiwan",
+    24.8066333,
+    120.9686833
+  ],
+  [
+    "Taipei, Taiwan",
+    25.0375198,
+    121.5636796
   ]
 ];
