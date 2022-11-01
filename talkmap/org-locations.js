b7f5dc01525abf37516bfe950811078e756bf7fb
Chih-Li Sung <iamdfchile@gmail.com>
2022-11-01 14:35:05 -0400

    update talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,9 +5,19 @@ var addressPoints = [
     -77.0365427
   ],
   [
+    "East Lansing, MI",
+    42.7320307,
+    -84.4721678
+  ],
+  [
     "Greensboro, North Carolina",
     36.0726355,
     -79.7919754
+  ],
+  [
+    "Los Angeles, CA",
+    34.0536909,
+    -118.242766
   ],
   [
     "Hsinchu, Taiwan",
